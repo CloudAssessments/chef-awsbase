f1bc4b8e689319ca147a24a5799c5dc3ac1475e5
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2017-09-01 14:01:19 -0500

    add amazone linux

--- cookbooks/bootstrap/recipes/default.rb
+++ cookbooks/bootstrap/recipes/default.rb
@@ -11,20 +11,44 @@ service 'sshd' do
   action :stop
 end
 
-user 'cloud_user' do
-  home '/home/cloud_user'
-  manage_home true
-  shell '/bin/bash'
-  password '$1$linuxaca$iGMxZ4g4lbPmfEDPhW3lw1'
-  salt 'linuxacademy'
-  gid 'sudo'
+if node['platform_family'] == "debian"
+
+  user 'cloud_user' do
+    home '/home/cloud_user'
+    manage_home true
+    shell '/bin/bash'
+    password '$1$linuxaca$iGMxZ4g4lbPmfEDPhW3lw1'
+    salt 'linuxacademy'
+    gid 'sudo'
+  end
+
+  group 'cloud_user' do
+    members 'cloud_user'
+  end
+  openssh_server '/etc/ssh/sshd_config' do
+    PasswordAuthentication yes
+  end
 end
 
-group 'cloud_user' do
-  members 'cloud_user'
-end
-openssh_server '/etc/ssh/sshd_config' do
-  PasswordAuthentication yes
+if node['platform_family'] == "amazon"
+  user 'cloud_user' do
+    home '/home/cloud_user'
+    manage_home true
+    shell '/bin/bash'
+    password '$1$linuxaca$iGMxZ4g4lbPmfEDPhW3lw1'
+    salt 'linuxacademy'
+  end
+
+  group 'cloud_user' do
+    members 'cloud_user'
+  end
+  openssh_server '/etc/ssh/sshd_config' do
+    PasswordAuthentication yes
+  end
+
+  execute 'add cloud_user to sudoers' do
+    command '/bin/echo \'cloud_user  ALL=(ALL)  NOPASSWD: ALL\' >> /etc/sudoers'
+  end
 end
 
 service 'sshd' do
